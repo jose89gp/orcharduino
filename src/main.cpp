bdea587f34657e398cdb4d51c118f75d4497ab94
Jose Antonio Garcia Perez <josse89@gmail.com>
2019-05-08 22:37:10 +0200

    Tested ultrasonic sensor library

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,20 @@
 #include <Arduino.h>
+#include <SR04.h>
+
+#define TRIG_PIN 12
+#define ECHO_PIN 11
+SR04 sr04 = SR04(ECHO_PIN,TRIG_PIN);
+long a;
+
 
 void setup() {
-  // put your setup code here, to run once:
-  //test code
+   Serial.begin(9600);
+   delay(1000);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+   a=sr04.Distance();
+   Serial.print(a);
+   Serial.println("cm");
+   delay(1000);
 }
