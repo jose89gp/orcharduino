abff566764d5ad0284a0e02f50acae30dea26d7e
Jose Antonio Garcia Perez <josse89@gmail.com>
2019-05-19 09:46:36 +0200

    Added RGB led and soil moisture sensor

--- src/main.cpp
+++ src/main.cpp
@@ -7,20 +7,32 @@
 
 #define TRIG_PIN 13
 #define ECHO_PIN 12
-#define DHTPIN 2 
+#define DHTPIN 2
 #define DHTTYPE    DHT11 
+#define RED 3
+#define GREEN 5
+#define BLUE 6
+
 SR04 sr04 = SR04(ECHO_PIN,TRIG_PIN);
 long a;
 long humidity;
 long temperature;
 DHT_Unified dht(DHTPIN, DHTTYPE);
-uint32_t delayMS = 2000;
+uint32_t delayMS = 500;
 // initialize the library with the numbers of the interface pins
-LiquidCrystal lcd(6,7,8,9,10,11);
+LiquidCrystal lcd(0,7,8,9,10,11);
 
 
 void setup() {
-   Serial.begin(9600);
+  pinMode(RED,OUTPUT);
+  pinMode(GREEN,OUTPUT);
+  pinMode(BLUE,OUTPUT);
+
+  digitalWrite(RED,LOW);
+  digitalWrite(GREEN, HIGH);
+  digitalWrite(BLUE,HIGH);
+  
+   //Serial.begin(9600);
    dht.begin();
    // Print temperature sensor details.
    sensor_t sensor;
@@ -39,7 +51,9 @@ void loop() {
    Serial.print(a);
    Serial.println("cm");
    delay(delayMS); 
-   
+   analogWrite(RED,rand()*255);
+   analogWrite(GREEN,rand()*255);
+   analogWrite(BLUE,rand()*255);
    sensors_event_t event;
    dht.temperature().getEvent(&event);
    if (isnan(event.temperature)) {
@@ -79,5 +93,7 @@ void loop() {
   lcd.print("D=");
   lcd.print(a);
   lcd.print("cm");
-  
+
+  lcd.print("   ");
+  lcd.print(analogRead(0));
 }
